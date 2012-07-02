3b9e2875ba8b5c09e27ab573715d67662106db4b
Felipe Coelho <fcoelho_9@hotmail.com>
2012-07-02 15:50:53 -0300

    mais exemplos

---
+++ exemplos/relatorio/while.pas
@@ -0,0 +1,15 @@
+program whileprog;
+    var x,y : integer;
+begin
+    {calcula o resto da divisao de x por y}
+    readln(x,y);
+    if x < 0 then
+        x := -x;
+    if y < 0 then
+        y := -y;
+    while x > y do
+    begin
+        x :=  x - y;
+    end;
+    writeln(x);
+end.
